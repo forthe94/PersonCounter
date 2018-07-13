786727467332fce1eb2c3664f8f852d955e94bc9
Александр Тетенков <tetenkov@tecon.ru>
2018-07-13 09:26:09 +0300

    little reorganisation

--- main/main.c
+++ main/main.c
@@ -45,13 +45,38 @@ SOFTWARE.
 #include "mdns.h"
 #include "lwip/api.h"
 #include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
 #include "lwip/netdb.h"
-#include "baremetal.h"
+#include "lwip/dns.h"
+#include "esp_sleep.h"
+#include "esp_deep_sleep.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "soc/rtc_cntl_reg.h"
+#include "soc/sens_reg.h"
+#include "driver/rtc_io.h"
+#include "esp32/ulp.h"
+#include "ulp_app.h"
+
+#include "main.h"
 #include "http_server.h"
 #include "wifi_manager.h"
 
+#define SSID "Tecon412"
+#define PASSPHARSE "Tecon412"
+#define MESSAGE "HelloTCPServer"
+#define TCPServerIP "192.168.51.132"
 
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+static const char *TAG="tcp_client";
 
+extern const uint8_t bin_start[] asm("_binary_ulp_app_bin_start");
+extern const uint8_t bin_end[]   asm("_binary_ulp_app_bin_end");
+
+static void init_ulp_program();
+static void update_pulse_count();
 
 static TaskHandle_t task_http_server = NULL;
 static TaskHandle_t task_wifi_manager = NULL;
@@ -79,29 +104,224 @@ void app_main()
 	/* initialize flash memory */
 	nvs_flash_init();
 
-	/* Look for saved sta config */
-	if(wifi_manager_fetch_wifi_sta_config())
-	{
-		/* if find one */
-		for(;;)
-			bareMetal();
+
+    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+    if (cause == ESP_SLEEP_WAKEUP_TIMER) {
+    	printf("Timer wake up, need to send pulse_count\n");
+	 	update_pulse_count();
+	    printf("Entering deep sleep\n\n");
+	    ESP_ERROR_CHECK(esp_sleep_enable_ulp_wakeup());
+		ESP_ERROR_CHECK(esp_deep_sleep_enable_timer_wakeup(5000000));
+		vTaskDelay(10);
+		esp_deep_sleep_start();
 	}
-	else
-	{
-	/* Not found go to wifi_manager */
+	else {
+		/* Look for saved sta config */
+		if(wifi_manager_fetch_wifi_sta_config())
+		{
+			/* if find one */
+			printf("Have wifi config so init ULP\n");
+			init_ulp_program();
+		    printf("Entering deep sleep\n\n");
+		    tcp_client_start();
+/*		    ESP_ERROR_CHECK(esp_sleep_enable_ulp_wakeup());
+			ESP_ERROR_CHECK(esp_deep_sleep_enable_timer_wakeup(5000000));
+			vTaskDelay(10);
+			esp_deep_sleep_start();*/
+		}
+		else
+		{
+		/* Not found go to wifi_manager */
 
-	/* start the HTTP Server task */
-	xTaskCreate(&http_server, "http_server", 2048, NULL, 5, &task_http_server);
+		/* start the HTTP Server task */
+		xTaskCreate(&http_server, "http_server", 2048, NULL, 5, &task_http_server);
 
-	/* start the wifi manager task */
-	xTaskCreate(&wifi_manager, "wifi_manager", 4096, NULL, 4, &task_wifi_manager);
+		/* start the wifi manager task */
+		xTaskCreate(&wifi_manager, "wifi_manager", 4096, NULL, 4, &task_wifi_manager);
 
-	/* your code should go here. In debug mode we create a simple task on core 2 that monitors free heap memory */
-#if WIFI_MANAGER_DEBUG
-	xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
-#endif
+		/* your code should go here. In debug mode we create a simple task on core 2 that monitors free heap memory */
+	#if WIFI_MANAGER_DEBUG
+		xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
+	#endif
+		}
 	}
+}/* End app_main() */
 
+void wifi_connect(){
+    wifi_config_t cfg = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASSPHARSE,
+        },
+    };
+    ESP_ERROR_CHECK( esp_wifi_disconnect() );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &cfg) );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+}
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+    esp_log_level_set("wifi", ESP_LOG_NONE); // disable wifi driver logging
+    tcpip_adapter_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+void tcp_client(void *pvParam){
+    ESP_LOGI(TAG,"tcp_client task started \n");
+    struct sockaddr_in tcpServerAddr;
+    tcpServerAddr.sin_addr.s_addr = inet_addr(TCPServerIP);
+    tcpServerAddr.sin_family = AF_INET;
+    tcpServerAddr.sin_port = htons( 3010 );
+    int s, r;
+    char recv_buf[64];
+    while(1){
+        xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,portMAX_DELAY);
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.\n");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket\n");
+         if(connect(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d \n", errno);
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... connected \n");
+        if( write(s , MESSAGE , strlen(MESSAGE)) < 0)
+        {
+            ESP_LOGE(TAG, "... Send failed \n");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
+        close(s);
+        ESP_LOGI(TAG, "... new request in 5 seconds");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+    ESP_LOGI(TAG, "...tcp_client task closed\n");
+}
+
+void tcp_client_start(void)
+{
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_event_group = xEventGroupCreate();
+    initialise_wifi();
+    xTaskCreate(&tcp_client,"tcp_client",4048,NULL,5,NULL);
+}
+
+
+
+
+
+static void init_ulp_program()
+{
+    esp_err_t err = ulp_load_binary(0, bin_start,
+            (bin_end - bin_start) / sizeof(uint32_t));
+    ESP_ERROR_CHECK(err);
+
+    /* Initialize some variables used by ULP program.
+     * Each 'ulp_xyz' variable corresponds to 'xyz' variable in the ULP program.
+     * These variables are declared in an auto generated header file,
+     * 'ulp_main.h', name of this file is defined in component.mk as ULP_APP_NAME.
+     * These variables are located in RTC_SLOW_MEM and can be accessed both by the
+     * ULP and the main CPUs.
+     *
+     * Note that the ULP reads only the lower 16 bits of these variables.
+     */
+    printf("edge_count = %d\n",ulp_edge_count);
+    ulp_wake_period = 25;
+    ulp_debounce_counter = 3;
+    ulp_debounce_max_count = 3;
+    ulp_next_edge = 0;
+    ulp_io_number = 11; /* GPIO0 is RTC_IO 11 */
+    ulp_edge_count_to_wake_up = 10;
+    /* Initialize GPIO0 as RTC IO, input, disable pullup and pulldown */
+    gpio_num_t gpio_num = GPIO_NUM_0;
+    rtc_gpio_set_direction(gpio_num, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(gpio_num);
+    rtc_gpio_pullup_dis(gpio_num);
+    rtc_gpio_hold_en(gpio_num);
+
+    /* Disconnect GPIO12 and GPIO15 to remove current drain through
+     * pullup/pulldown resistors.
+     * GPIO15 may be connected to ground to suppress boot messages.
+     * GPIO12 may be pulled high to select flash voltage.
+     */
+    rtc_gpio_isolate(GPIO_NUM_12);
+    rtc_gpio_isolate(GPIO_NUM_15);
+
+    /* Set ULP wake up period to T = 20ms (3095 cycles of RTC_SLOW_CLK clock).
+     * Minimum pulse width has to be T * (ulp_debounce_counter + 1) = 80ms.
+     */
+    REG_SET_FIELD(SENS_ULP_CP_SLEEP_CYC0_REG, SENS_SLEEP_CYCLES_S0, 3095);
+
+    /* Start the program */
+    err = ulp_run((&ulp_entry - RTC_SLOW_MEM) / sizeof(uint32_t));
+    ESP_ERROR_CHECK(err);
+}
+
+static void update_pulse_count()
+{
+    const char* namespace = "plusecnt";
+    const char* count_key = "count";
+    printf("edge_count = %d\n",ulp_edge_count& UINT16_MAX );
+    printf("ulp_wake_time = %d\n",ulp_wake_time & UINT16_MAX );
+    printf("ulp_wake_set = %d\n",ulp_wake_set & UINT16_MAX );
+    printf("ulp_test = %d\n",ulp_test & UINT16_MAX );
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    nvs_handle handle;
+    ESP_ERROR_CHECK( nvs_open(namespace, NVS_READWRITE, &handle));
+    uint32_t pulse_count = 0;
+    esp_err_t err = nvs_get_u32(handle, count_key, &pulse_count);
+    assert(err == ESP_OK || err == ESP_ERR_NVS_NOT_FOUND);
+    printf("Read pulse count from NVS: %5d\n", pulse_count);
+
+    /* ULP program counts signal edges, convert that to the number of pulses */
+    uint32_t pulse_count_from_ulp = (ulp_edge_count & UINT16_MAX) / 2;
+    /* In case of an odd number of edges, keep one until next time */
+    ulp_edge_count = ulp_edge_count % 2;
+    printf("Pulse count from ULP: %5d\n", pulse_count_from_ulp);
+
+    /* Save the new pulse count to NVS */
+    pulse_count += pulse_count_from_ulp;
+    ESP_ERROR_CHECK(nvs_set_u32(handle, count_key, pulse_count));
+    ESP_ERROR_CHECK(nvs_commit(handle));
+    nvs_close(handle);
+    printf("Wrote updated pulse count to NVS: %5d\n", pulse_count);
 }
 
 
---
+++ main/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 11, 2018
+ *      Author: tetenkov
+ */
+
+#ifndef WIFI_MANAGER_MAIN_MAIN_H_
+#define WIFI_MANAGER_MAIN_MAIN_H_
+
+void tcp_client_start(void);
+
+
+
+#endif /* WIFI_MANAGER_MAIN_MAIN_H_ */
